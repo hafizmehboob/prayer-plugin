65b731981066162ff28830b379bee5a75f33d400
hafizmehboob <hafizabubakar.mehboob@gmail.com>
2021-10-03 15:59:17 +0500

    Added PLugin Detiails

--- prayers-timings.php
+++ prayers-timings.php
@@ -1,1 +1,12 @@
-<?php ?>
+<?php
+/**
+* Plugin Name: Prayers Timings
+* Plugin URI: http://www.glowing-tips.com/
+* Description: This is the prayers timings plugin based on aladhan API.
+* Version: 1.0
+* Author: Hafiz Abubakar Mehboob
+* Author URI: http://glowing-tips.com/
+**/
+
+
+ ?>
