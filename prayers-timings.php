83fd82e4a72d31e7be355b01bf71f13df45bd269
hafizmehboob <hafizabubakar.mehboob@gmail.com>
2021-10-12 20:56:48 +0500

    working on get cities on country base

--- prayers-timings.php
+++ prayers-timings.php
@@ -58,28 +58,5 @@ function my_cool_plugin_settings_page(){
     </div>
 <?php 
 
-$curl = curl_init();
-curl_setopt_array($curl, array(
-CURLOPT_URL => 'https://countriesnow.space/api/v0.1/countries/cities',
-CURLOPT_RETURNTRANSFER => true,
-CURLOPT_ENCODING => '',
-CURLOPT_MAXREDIRS => 10,
-CURLOPT_TIMEOUT => 0,
-CURLOPT_FOLLOWLOCATION => true,
-CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-CURLOPT_CUSTOMREQUEST => 'POST',
-CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
-CURLOPT_POSTFIELDS =>'{
-"country": "pakistan"
-}',
-));
-
-$response = curl_exec($curl);
-
-curl_close($curl);
-$response = json_decode($response);
-$getCititesList = $response->data;
-
-
 } 
 ?>
